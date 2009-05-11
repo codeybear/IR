51edfc7798184352b0747436106ad1720f4ff335
pjcooke_cp <SND\pjcooke_cp>
2009-05-11 12:10:18 +0000

    Updated with missing files from recent changes.

---
+++ Forms/frmShowImage.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ImageSearch
+{
+    partial class frmShowImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.btnShowDetail = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImage
+            // 
+            this.picImage.Location = new System.Drawing.Point(12, 12);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(764, 562);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picImage.TabIndex = 0;
+            this.picImage.TabStop = false;
+            // 
+            // btnShowDetail
+            // 
+            this.btnShowDetail.Location = new System.Drawing.Point(1, 0);
+            this.btnShowDetail.Name = "btnShowDetail";
+            this.btnShowDetail.Size = new System.Drawing.Size(25, 23);
+            this.btnShowDetail.TabIndex = 1;
+            this.btnShowDetail.Text = "->";
+            this.btnShowDetail.UseVisualStyleBackColor = true;
+            this.btnShowDetail.Click += new System.EventHandler(this.btnShowDetail_Click);
+            // 
+            // frmShowImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(788, 606);
+            this.Controls.Add(this.btnShowDetail);
+            this.Controls.Add(this.picImage);
+            this.Name = "frmShowImage";
+            this.Text = "frmShowImage";
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.Button btnShowDetail;
+    }
+}
---
+++ Forms/frmShowImage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageSearch
+{
+    public partial class frmShowImage : Form
+    {
+        string _sFile;
+
+        /// <summary>
+        /// Display the image
+        /// </summary>
+        /// <param name="sFile">Image file and path</param>
+        public frmShowImage(string sFile)
+        {
+            InitializeComponent();
+
+            _sFile = sFile;
+            picImage.Image = new Bitmap(_sFile);
+            this.Text = _sFile;
+        }
+
+        private void btnShowDetail_Click(object sender, EventArgs e)
+        {
+            IR ir = new IR();
+
+            picImage.Image = ir.GetTestImage(_sFile);
+        }
+    }
+}
